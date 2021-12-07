c929cf4a052a4e9978d9fba51c6631c41744b721
ms-choudhary <ms.choudhary2000@gmail.com>
2021-12-07 18:53:25 +0530

    run sops from env

--- downloader.sh
+++ downloader.sh
@@ -3,7 +3,7 @@
 # The defined command will be invoked with the following scheme: command certFile keyFile caFile full-URL
 full_url=${4}
 
-if ! sops -d "$(basename $full_url)"; then
+if ! ${SOPS_BIN_PATH} -d "$(basename $full_url)"; then
   >&2 echo "failed to decrypt secrets"
   exit 1
 fi
